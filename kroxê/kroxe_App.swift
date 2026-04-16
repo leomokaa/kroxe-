50c5cdbd5c5209cf4a6607cbbf6379c1ae22eb86
leomokaa <kaleohenriquemonteiro@gmail.com>
2026-04-16 09:24:14 -0300

    Initial Commit

---
+++ kroxê/kroxe_App.swift
@@ -0,0 +1,17 @@
+//
+//  kroxe_App.swift
+//  kroxê
+//
+//  Created by User on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct kroxe_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
