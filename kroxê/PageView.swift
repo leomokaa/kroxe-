8f490ff904b2c38d05bf05b6a7904bfcfe4b0826
leomokaa <kaleohenriquemonteiro@gmail.com>
2026-04-23 11:20:30 -0300

    feat: Criacao do forms

---
+++ kroxê/PageView.swift
@@ -0,0 +1,26 @@
+//
+//  PageView.swift
+//  kroxê
+//
+//  Created by User on 17/04/26.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    
+    var body: some View {
+        NavigationStack {
+            VStack {}
+            }
+            .navigationTitle("Minhas Receitas")
+        
+    }
+}
+
+
+
+
+#Preview {
+        PageView ()
+}
--- kroxê/kroxe_App.swift
+++ kroxê/kroxe_App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct kroxe_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PageView()
         }
     }
 }
