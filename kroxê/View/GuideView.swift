4566095075a2fd055efce9050a495cf7a60bcf67
amandafnsc <amandacoelho08.ac@gmail.com>
2026-04-28 08:30:16 -0300

    feat: recipes list and tab view

---
+++ kroxê/View/GuideView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideView.swift
+//  kroxê
+//
+//  Created by User on 27/04/26.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideView()
+}
---
+++ kroxê/View/RecipesListView.swift
@@ -0,0 +1,44 @@
+//
+//  RecipesListView.swift
+//  kroxê
+//
+//  Created by User on 27/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct RecipesListView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \Recipe.name) var recipes: [Recipe] = [
+        Recipe(name: "Coelhinho", link: "", yarn: 2, needle: 3.5, text: "lalala"),
+        Recipe(name: "Estrelinha", link: "blablbla", yarn: 1, needle: 2.0, text: "fafafa")
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            VStack (alignment: .leading, spacing: 20) {
+                Text("Suas receitas de crochê em um só lugar")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                VStack (spacing: 14) {
+                    ForEach(recipes.enumerated(), id: \.offset) { index, recipe in
+                        CardRecipeView(recipe: recipe)
+                    }
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal)
+            .padding(.top, -6)
+            .navigationTitle("Receitas")
+            
+            .backgroundCream()
+        }
+    }
+}
+
+#Preview {
+    return RecipesListView()
+        .modelContainer(PreviewSetup.previewRecepiesList)
+}
