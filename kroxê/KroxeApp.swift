e168df367bca8d9b1dbe3c32a400f693a5d9c39e
amandafnsc <amandacoelho08.ac@gmail.com>
2026-04-30 16:08:42 -0300

    style: sheet

--- kroxê/KroxeApp.swift
+++ kroxê/KroxeApp.swift
@@ -23,8 +23,41 @@ struct kroxe_App: App {
             guard isFirstLaunch else { return }
             do {
                 let modelContext = try $0.get().mainContext
-                modelContext.insert(Recipe(name: "Coelhinho", link: "", yarn: 2, needle: 3.5, text: "lalala", counter: 0))
-                modelContext.insert(Recipe(name: "Estrelinha", link: "https://apple.com", yarn: 1, needle: 2.0, text: "fafafa", counter: 0))
+                modelContext.insert(Recipe(name: "Coelhinho", link: "", yarn: 2, needle: 3.5, text: """
+            #Cabeça
+            Rnd 1. 7 sc in magic ring (7)
+            Rnd 2. 7 inc (14)
+            Rnd 3. (1 sc, inc) x 7 (21)
+            Rnd 4. (2 sc, inc) x 7 (28)
+            Rnd 5. (3 sc, inc) x 7 (35)
+            Rnd 6 - 13. sc in each (35)
+            Add safety eyes between rounds 10 and 11, 7 st apart
+            Stuff
+            Rnd 14. (3 sc, dec) x 7 (28)
+            Rnd 15. (2 sc, dec) x 7 (21)
+            Rnd 16. (1 sc, dec) x 7 (14)
+            Stuff some more
+            Rnd 17. 14 dec (7)
+            sl st and f.o.
+            Embroider a nose between rounds 10 and 11, mouth from 11  to 12
+            
+            #Ears (make 2)
+            Rnd 1. 6 sc in magic ring (6)
+            Rnd 2. 6 inc (12)
+            Rnd 3. (2 sc, inc) x 4 (16)
+            Rnd 4 - 9. sc in each (16)
+            Rnd 10. (6 sc, dec) x 2 (14)
+            Rnd 11 - 13. sc in each (14)
+            Rnd 14. (5 sc, dec) x 2 (12)
+            Rnd 15 - 16. sc in each (12)
+            Rnd 17. (4 sc, dec) x 2 (10)
+            Rnd 18. sc in each (10)
+            If you prefer longer you can add 1-3 rounds
+            sl st and f.o., leave long tail to sew
+            Don't stuff!
+            Sew an ear to each side of head, between rounds 5 and 6
+            """,
+                   counter: 0))
                 isFirstLaunch = false
             } catch {
                 
--- kroxê/View/Components/ItemRecipeStitchView.swift
+++ kroxê/View/Components/ItemRecipeStitchView.swift
@@ -11,34 +11,30 @@ struct CardRecipeStitchView: View {
     var stitch: Stitch
     @State var shouldPresentSheet: Bool = false
     @State private var sheetHeight: CGFloat = .zero
-    var cardWidth: CGFloat = 67
     
     var body: some View {
         VStack (spacing: 4) {
             VStack{
                 Button (action: {shouldPresentSheet.toggle() }) {
                     Image(stitch.image)
-                        .padding(10.0)
                 }
-                .frame(width: cardWidth, height: 73)
+                .frame(width: 67, height: 73)
                 .sheet(isPresented: $shouldPresentSheet) {
-                    print("Sheet dismissed!")
                 } content: {
-                    CardStitchView(stitch: stitch)
-                        .presentationDetents([.height(200)])
+                    StitchSheetView(stitch: stitch)
+                        .presentationDetents([.height(310)])
                 }
             }
             .padding(.horizontal, 6)
             .padding(.vertical, 4)
             .background(Color.lightRosaSuave, in: RoundedRectangle(cornerRadius: 10))
             
-            Text(stitch.abreviatedName)
+            Text(stitch.name)
                 .multilineTextAlignment(.center)
                 .font(.caption.bold())
                 .foregroundColor(.ameixa)
                 .frame(maxWidth: .infinity)
         }
-        .frame(width: cardWidth + 10)
     }
 }
 
