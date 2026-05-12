5996aafe83e40e3570aef71785c33756c4d20beb
Marina Gomes Fontenele <marinagomesfontenele2007@gmail.com>
2026-05-12 11:52:46 -0300

    feat: sheet create and edit

--- kroxê/View/RecipeView.swift
+++ kroxê/View/RecipeView.swift
@@ -20,113 +20,131 @@ import SwiftData
 //    }
 //}
 
-struct RecipeView: View {
-    
-    @Environment(\.modelContext)
-    var modelContext //modelContext.delete(model)
-    @Environment(\.dismiss) private var dismiss
-    
+struct ButtonInMenu: View {
+    @State var PresentSheet = false
     var recipe: Recipe
-    @State var bool: Bool = true
-    @State private var isDeleting: Bool = false
-    
-    @Namespace private var namespace
     
     var body: some View {
-        ScrollView(showsIndicators: false){
-            VStack(alignment: .leading, spacing: 20) {
-                VStack(alignment: .leading, spacing: 12) {
-                    VStack{
-                        if let data = recipe.photo {
-                            if let uiImage = UIImage(data: data) {
-                                HStack(alignment: .center){
-                                    NavigationLink {
-                                        Image(uiImage: uiImage)
-                                            .resizable()
-                                            .scaledToFit()
-                                            .navigationTransition(.zoom(sourceID: "zoom", in: namespace))
-                                    } label: {
-                                        Image(uiImage: uiImage)
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(maxWidth: .infinity, maxHeight: 148)
-                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+        Button ("Editar Receita", systemImage: "pencil.line") {
+            PresentSheet.toggle()
+        }
+        .buttonStyle(.borderedProminent)
+        .sheet(isPresented: $PresentSheet) {
+            NavigationStack {
+                EditRecipeView(recipe:recipe)
+            }
+        }
+    }
+}
+    
+    struct RecipeView: View {
+        
+        @Environment(\.modelContext)
+        var modelContext //modelContext.delete(model)
+        @Environment(\.dismiss) private var dismiss
+        @State var PresentSheet = false
+        
+        var recipe: Recipe
+        @State var bool: Bool = true
+        @State private var isDeleting: Bool = false
+        
+        @Namespace private var namespace
+        
+        var body: some View {
+            ScrollView(showsIndicators: false){
+                VStack(alignment: .leading, spacing: 20) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        VStack{
+                            if let data = recipe.photo {
+                                if let uiImage = UIImage(data: data) {
+                                    HStack(alignment: .center){
+                                        NavigationLink {
+                                            Image(uiImage: uiImage)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .navigationTransition(.zoom(sourceID: "zoom", in: namespace))
+                                        } label: {
+                                            Image(uiImage: uiImage)
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(maxWidth: .infinity, maxHeight: 148)
+                                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        }
                                     }
+                                    .frame(maxWidth: .infinity)
+                                }
+                            } else {
+                                HStack(alignment: .center) {
+                                    Image("RecipeImageDefault")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(maxWidth: .infinity, maxHeight: 148)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
                                 }
                                 .frame(maxWidth: .infinity)
                             }
-                        } else {
-                            HStack(alignment: .center) {
-                                Image("RecipeImageDefault")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(maxWidth: .infinity, maxHeight: 148)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                            }
-                            .frame(maxWidth: .infinity)
                         }
-                    }
-                    
-                    Text(recipe.name)
-                        .font(.title.bold())
-                        .foregroundStyle(.ameixa)
-                    
-                    if (!(recipe.needle == 0) || !(recipe.yarn == 0)) {
-                        HStack (spacing: 8){
-                            if (!(recipe.needle == 0)) {
-                                TagRecipeView(tagIcon: "wand.and.outline", tagName: "Agulha de \(recipe.needle.formatted(.number.precision(.fractionLength(1)))) mm")
-                            }
-                            
-                            if (!(recipe.yarn == 0)) {
-                                if (!(recipe.yarn == 1)) {
-                                    TagRecipeView(tagIcon: "circle", tagName: "\(recipe.yarn) Novelos")
+                        
+                        Text(recipe.name)
+                            .font(.title.bold())
+                            .foregroundStyle(.ameixa)
+                        
+                        if (!(recipe.needle == 0) || !(recipe.yarn == 0)) {
+                            HStack (spacing: 8){
+                                if (!(recipe.needle == 0)) {
+                                    TagRecipeView(tagIcon: "wand.and.outline", tagName: "Agulha de \(recipe.needle.formatted(.number.precision(.fractionLength(1)))) mm")
                                 }
-                                else {
-                                    TagRecipeView(tagIcon: "circle", tagName: "\(recipe.yarn) Novelo")
+                                
+                                if (!(recipe.yarn == 0)) {
+                                    if (!(recipe.yarn == 1)) {
+                                        TagRecipeView(tagIcon: "circle", tagName: "\(recipe.yarn) Novelos")
+                                    }
+                                    else {
+                                        TagRecipeView(tagIcon: "circle", tagName: "\(recipe.yarn) Novelo")
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                
-                //para mostrar os pontos
-                ScrollView(.horizontal, showsIndicators: false){
-                    HStack(alignment: .top){
-                        ForEach(stitchs(in: recipe.text)) { stitch in
-                            CardRecipeStitchView(stitch: stitch)
+                    
+                    //para mostrar os pontos
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(alignment: .top){
+                            ForEach(stitchs(in: recipe.text)) { stitch in
+                                CardRecipeStitchView(stitch: stitch)
+                            }
                         }
                     }
-                }
-                
-                Text(
-                    hightlighTitles(
-                        highlightKeywords(recipe.text)
+                    
+                    Text(
+                        hightlighTitles(
+                            highlightKeywords(recipe.text)
+                        )
                     )
-                )
-                .lineSpacing(6)
-                .foregroundStyle(.ameixa)
-                //                .overlay (
-                //                    Image (systemName: "hand. thumbsup-fill")
-                //                        .font(.system(size: 200))
-                //                        .opacity(0.7)
-                //                        .frame(width: 450, height: 300, alignment: .bottom)
-                //                        .glassEffect()
-                //                )
+                    .lineSpacing(6)
+                    .foregroundStyle(.ameixa)
+                    //                .overlay (
+                    //                    Image (systemName: "hand. thumbsup-fill")
+                    //                        .font(.system(size: 200))
+                    //                        .opacity(0.7)
+                    //                        .frame(width: 450, height: 300, alignment: .bottom)
+                    //                        .glassEffect()
+                    //                )
+                }
+                .padding(.top, 10)
+                .padding(.bottom, 170)
+                .padding(.horizontal)
             }
-            .padding(.top, 10)
-            .padding(.bottom, 170)
-            .padding(.horizontal)
-        }
-        
-        //contador
-        .overlay (alignment: .bottom) {
-            RecipeCounterView(recipe: recipe)
-        }
-        //        .sheet(isPresented: $bool, content: {
-        //
-        //                })
-        //                    .padding()
-        //                    .frame(width: 40, height: 30)
+            
+            //contador
+            .overlay (alignment: .bottom) {
+                RecipeCounterView(recipe: recipe)
+            }
+            //        .sheet(isPresented: $bool, content: {
+            //
+            //                })
+            //                    .padding()
+            //                    .frame(width: 40, height: 30)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Menu(content: {
@@ -137,10 +155,10 @@ struct RecipeView: View {
                             })
                         }
                         
-                        NavigationLink(destination: EditRecipeView(recipe: recipe)) {
-                            Image(systemName: "pencil.line")
-                            Text("Editar Receita")
+                        Button ("Editar Receita", systemImage: "pencil.line") {
+                            PresentSheet.toggle()
                         }
+                        .buttonStyle(.borderedProminent)
                         
                         Button("Excluir Receita", systemImage: "trash", role: .destructive) {
                             isDeleting = true
@@ -148,6 +166,11 @@ struct RecipeView: View {
                         }
                         
                     }, label: {Image(systemName: "ellipsis")})
+                }
+            }
+            .sheet(isPresented: $PresentSheet) {
+                NavigationStack {
+                    EditRecipeView(recipe: recipe)
                 }
             }
             .backgroundCream()
@@ -164,115 +187,116 @@ struct RecipeView: View {
             }, message: {
                 Text("Tem certeza que deseja excluir essa receita?")
             })
-    }
-    
-    
-    //olha os pontos que estão no texto
-    func stitchs(in string: String) -> [Stitch] {
-        let allNames = Stitch.allCases.flatMap(\.allNames)
-        let filteredNames = allNames.filter { keyword in
-            string.range(of: "(\\b\(keyword)\\b|(?<=\\d)\(keyword)\\b)", options: [.regularExpression, .caseInsensitive]) != nil
-        }
-        let stitchs = filteredNames.compactMap { name in
-            Stitch(name: name)
-        }
-        let uniqueStitchs = OrderedSet(stitchs)
-        return Array(uniqueStitchs)
-    }
-    
-    //deixa negrito os pontos
-    func highlightKeywords(_ string: String) -> String {
-        let keywords = Stitch.allCases.flatMap(\.allNames)
-        var result = string
-        for keyword in keywords {
-            result = result.replacingOccurrences(of: "(\\b\(keyword)\\b|(?<=\\d)\(keyword)\\b)", with: "**\(keyword)**", options: [.regularExpression, .caseInsensitive])
-        }
-        return result
-    }
-    
-    //deixa negrito os titulos
-    func hightlighTitles(_ string: String) -> AttributedString {
-        var titles = string.components(separatedBy: .newlines).filter { line in
-            line.starts(with: "#")
-        }
-        titles = titles.map { title in
-            String(title.dropFirst())
         }
         
-        let string = string.replacingOccurrences(of: "#", with: "")
         
-        var attributedString = try! AttributedString(
-            markdown: string,
-            options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)
-        )
-        
-        for title in titles {
-            if let range = attributedString.range(of: title) {
-                attributedString[range].font = .title2
-                attributedString[range].inlinePresentationIntent = .stronglyEmphasized
+        //olha os pontos que estão no texto
+        func stitchs(in string: String) -> [Stitch] {
+            let allNames = Stitch.allCases.flatMap(\.allNames)
+            let filteredNames = allNames.filter { keyword in
+                string.range(of: "(\\b\(keyword)\\b|(?<=\\d)\(keyword)\\b)", options: [.regularExpression, .caseInsensitive]) != nil
             }
+            let stitchs = filteredNames.compactMap { name in
+                Stitch(name: name)
+            }
+            let uniqueStitchs = OrderedSet(stitchs)
+            return Array(uniqueStitchs)
         }
         
-        return attributedString
+        //deixa negrito os pontos
+        func highlightKeywords(_ string: String) -> String {
+            let keywords = Stitch.allCases.flatMap(\.allNames)
+            var result = string
+            for keyword in keywords {
+                result = result.replacingOccurrences(of: "(\\b\(keyword)\\b|(?<=\\d)\(keyword)\\b)", with: "**\(keyword)**", options: [.regularExpression, .caseInsensitive])
+            }
+            return result
+        }
+        
+        //deixa negrito os titulos
+        func hightlighTitles(_ string: String) -> AttributedString {
+            var titles = string.components(separatedBy: .newlines).filter { line in
+                line.starts(with: "#")
+            }
+            titles = titles.map { title in
+                String(title.dropFirst())
+            }
+            
+            let string = string.replacingOccurrences(of: "#", with: "")
+            
+            var attributedString = try! AttributedString(
+                markdown: string,
+                options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)
+            )
+            
+            for title in titles {
+                if let range = attributedString.range(of: title) {
+                    attributedString[range].font = .title2
+                    attributedString[range].inlinePresentationIntent = .stronglyEmphasized
+                }
+            }
+            
+            return attributedString
+        }
+        
+        //    func isValidUrl(string: String) -> Bool {
+        //        let urlRegEx = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
+        //        let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
+        //        let result = urlTest.evaluate(with: url)
+        //        return result
+        //    }
+        
     }
     
-//    func isValidUrl(string: String) -> Bool {
-//        let urlRegEx = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
-//        let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
-//        let result = urlTest.evaluate(with: url)
-//        return result
-//    }
-    
-}
-
-#Preview {
-    RecipeView(
-        recipe: Recipe(
-            name: "Minha Primeira Receita",
-            link: "",
-            yarn: 0,
-            needle: 0.0,
-            text: """
+    #Preview {
+        RecipeView(
+            recipe: Recipe(
+                name: "Minha Primeira Receita",
+                link: "",
+                yarn: 0,
+                needle: 0.0,
+                text: """
         #Receita
         Faça um nó inicial
         Carreira 1: 6 corr [10]
         Carreira 2: 6 pb [10]
         Carreira 3: 3 [sc, inc] [9]
-
+        
         #Dicas
         Use # no início da linha para escrever um título e dividir sua receita em partes.
-
+        
         Você pode escrever os pontos como preferir, por extenso ou em abreviações, em português ou inglês:
         “pb”, “ponto baixo”, “sc” ou “single crochet”.
         Os pontos detectados são automaticamente listados no topo da receita.
-
+        
         Use o contador de carreiras para acompanhar seu projeto enquanto faz.
         """,
-            counter: 0
+                counter: 0
+            )
         )
-    )
-}
+    }
+    
+    //struct TitleHighlightView: View {
+    //
+    //    let string = """
+    //            #TITLE 1
+    //            aaaaaaa
+    //            aaaaaaa
+    //            aaaaaaa
+    //
+    //            #TITLE 2
+    //            bbbbbbb
+    //            bbbbbbb
+    //            bbbbbbb
+    //            """
+    //
+    //    var body: some View {
+    //        Text(hightlighted())
+    //    }
+    //
+    //}
+    //
+    //#Preview {
+    //    TitleHighlightView()
+    //}
 
-//struct TitleHighlightView: View {
-//
-//    let string = """
-//            #TITLE 1
-//            aaaaaaa
-//            aaaaaaa
-//            aaaaaaa
-//
-//            #TITLE 2
-//            bbbbbbb
-//            bbbbbbb
-//            bbbbbbb
-//            """
-//
-//    var body: some View {
-//        Text(hightlighted())
-//    }
-//
-//}
-//
-//#Preview {
-//    TitleHighlightView()
-//}
