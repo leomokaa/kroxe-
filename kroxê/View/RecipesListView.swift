a5b9998a8285f3f9fcab72cb8d57614cfaea6d36
Marina Gomes Fontenele <marinagomesfontenele2007@gmail.com>
2026-07-28 12:00:58 -0300

    feat: menu and swipe

--- kroxê/View/RecipesListView.swift
+++ kroxê/View/RecipesListView.swift
@@ -16,27 +16,36 @@ struct RecipesListView: View {
     @Environment(\.modelContext) private var modelContext
     @Query(sort: \Recipe.timestamp, order: .reverse) var recipes: [Recipe]
     @State var PresentSheet = false
+    @State var PresentEditSheet: Bool = false
+    @State private var isDeleting: Bool = false
+    //  @State var recipe: Recipe
     
     var body: some View {
         NavigationStack {
             ZStack {
-                ScrollView(showsIndicators: false){
-                    if horizontalSizeClass == .compact {
-                        VStack (spacing: 14) {
-                            recipiesItems
-                        }
-                        .padding(.horizontal)
-                        .padding(.vertical, 20)
-                    } else {
-                        LazyVGrid(columns: [GridItem(spacing: 14), GridItem()], spacing: 14) {
-                            recipiesItems
-                        }
-                        .padding(.horizontal)
-                        .padding(.vertical, 20)
+                //                ScrollView(showsIndicators: false){
+                if horizontalSizeClass == .compact {
+                    VStack (spacing: 14) {
+                        iphoneItems
                     }
+                    .opacity(recipes.isEmpty ? 0 : 1)
+                    .padding(.horizontal)
+                    .padding(.vertical, 20)
                     
+                } else {
+                    //                        LazyVGrid(columns: [GridItem(spacing: 14), GridItem()], spacing: 14) {
+                    ipadItems
+                    //                        }
+                        .opacity(recipes.isEmpty ? 0 : 1)
+                        .padding(.horizontal)
+                        .padding(.vertical, 20)
+                        .listRowBackground(EmptyView())
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                 }
-                .opacity(recipes.isEmpty ? 0 : 1)
+                
+                //                }
+                //                .opacity(recipes.isEmpty ? 0 : 1)
                 
                 emptyStateList
                     .opacity(recipes.isEmpty ? 1 : 0)
@@ -50,6 +59,7 @@ struct RecipesListView: View {
                         PresentSheet.toggle()
                     }
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel(Text("Adicionar receita"))
                     .sheet(isPresented: $PresentSheet) {
                         NavigationStack {
                             CreateRecipeView()
@@ -75,12 +85,6 @@ struct RecipesListView: View {
             //                }
             //            }
         }
-        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
-            Button(role: .destructive) {
-            } label: {
-                Label("Apagar", systemImage: "trash")
-            }
-        }
         
         
         
@@ -93,20 +97,154 @@ struct RecipesListView: View {
         
     }
     
-    var recipiesItems: some View {
-            //                Text("Suas receitas de crochê em um só lugar")
-            //                    .font(.subheadline)
-            //                    .foregroundColor(.secondary)
-            ForEach(recipes.enumerated(), id: \.offset) { index, recipe in
-                CardRecipeView(recipe: recipe)
-            }
-            .swipeActions(edge: .trailing, allowsFullSwipe: false) {
-                Button(role: .destructive) {
-                } label: {
-                    Label("Apagar", systemImage: "trash")
+    var ipadItems: some View {
+        //                Text("Suas receitas de crochê em um só lugar")
+        //                    .font(.subheadline)
+        //                    .foregroundColor(.secondary)
+        ScrollView (showsIndicators: false) {
+            LazyVGrid(columns: [GridItem(spacing: 14), GridItem()], spacing: 14) {
+                ForEach(recipes.enumerated(), id: \.offset) { index, recipe in
+                    CardRecipeView(recipe: recipe)
+                        .listRowBackground(EmptyView())
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        .navigationLinkIndicatorVisibility(.hidden)
+//                        .sheet(isPresented: $PresentEditSheet) {
+//                            NavigationStack {
+//                                EditRecipeView(recipe: recipe)
+//                            }
+//                            .presentationSizing(.page)
+//                        }
+//                        .alert("Excluir receita", isPresented: $isDeleting, actions: {
+//                            HStack {
+//                                Button("Cancelar", role: .cancel) {
+//                                }
+//                                
+//                                Button("Excluir", role: .destructive) {
+//                                    modelContext.delete(recipe)
+//                                    dismiss()
+//                                }
+//                            }
+//                        }, message: {
+//                            Text("Tem certeza que deseja excluir essa receita?")
+//                        })
+//                        .contextMenu(menuItems: {
+//                            if let link = URL(string: recipe.link) {
+//                                Link(destination: link, label: {
+//                                    Image(systemName: "link")
+//                                    Text("Acessar o tutorial")})
+//                            }
+//                            
+//                            Button ("Editar Receita", systemImage: "pencil.line") {
+//                                PresentEditSheet.toggle()
+//                            }
+//                            .buttonStyle(.borderedProminent)
+//                            
+//                            Button("Excluir Receita", systemImage: "trash", role: .destructive) {
+//                                isDeleting = true
+//                            }
+//                        })
                 }
             }
+        }
         //        .scrollBounceBehavior()
+    }
+    
+    var iphoneItems: some View {
+        List{
+            ForEach(recipes.enumerated(), id: \.offset) { index, recipe in
+                CardRecipeView(recipe: recipe)
+                    .listRowBackground(EmptyView())
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .navigationLinkIndicatorVisibility(.hidden)
+                    .padding (.vertical, 7)
+//                    .sheet(isPresented: $PresentEditSheet) {
+//                        NavigationStack {
+//                            EditRecipeView(recipe: recipe)
+//                        }
+//                        .presentationSizing(.page)
+//                    }
+//                    .alert("Excluir receita", isPresented: $isDeleting, actions: {
+//                        HStack {
+//                            Button("Cancelar", role: .cancel) {
+//                            }
+//                            
+//                            Button("Excluir", role: .destructive) {
+//                                modelContext.delete(recipe)
+//                                dismiss()
+//                            }
+//                        }
+//                    }, message: {
+//                        Text("Tem certeza que deseja excluir essa receita?")
+//                    })
+//                    .contextMenu(menuItems: {
+//                        if let link = URL(string: recipe.link) {
+//                            Link(destination: link, label: {
+//                                Image(systemName: "link")
+//                                Text("Acessar o tutorial")})
+//                        }
+//                        
+//                        Button ("Editar Receita", systemImage: "pencil.line") {
+//                            PresentEditSheet.toggle()
+//                        }
+//                        .buttonStyle(.borderedProminent)
+//                        
+//                        Button("Excluir Receita", systemImage: "trash", role: .destructive) {
+//                            isDeleting = true
+//                        }
+//                    })
+//
+                
+                
+                
+                
+//                    .sheet(isPresented: $PresentEditSheet) {
+//                        NavigationStack {
+//                             EditRecipeView(recipe: recipe)
+//                        }
+//                        .presentationSizing(.page)
+//                    }
+//                    .alert("Excluir receita", isPresented: $isDeleting, actions: {
+//                        HStack {
+//                            Button("Cancelar", role: .cancel) {
+//                            }
+//                            
+//                            Button("Excluir", role: .destructive) {
+//                                modelContext.delete(recipe)
+//                                dismiss()
+//                            }
+//                        }
+//                    }, message: {
+//                        Text("Tem certeza que deseja excluir essa receita?")
+//                    })
+//                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+//                        Button(role: .destructive) {
+//                            isDeleting = true
+//                        } label: {
+//                            Label("Excluir", systemImage: "trash")
+//                        }
+//                        
+//                        Button {
+//                            PresentEditSheet.toggle()
+//                        } label: {
+//                            Label("Editar", systemImage: "pencil")
+//                        }
+//                        .tint(.accent)
+//                    }
+//                    
+//                    
+//                    .swipeActions(edge: .leading, allowsFullSwipe: false) {
+//                        if let link = URL(string: recipe.link) {
+//                            Link(destination: link, label: {
+//                                Image(systemName: "link")
+//                                Text("Acessar o tutorial")})
+//                        }
+//                    }
+            }
+        }
+        .listStyle(.plain)
+        // .opacity(albumsModel.isEmpty ? 0 : 1)
     }
     
     var emptyStateList: some View {
